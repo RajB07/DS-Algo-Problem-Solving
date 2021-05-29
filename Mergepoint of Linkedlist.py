1fd036e7c6880dae363a7c6ea43de7ab6394b015
Rajeev Bandi <62938640+RajB07@users.noreply.github.com>
2021-05-29 23:44:56 +0530

    Add files via upload

---
+++ Mergepoint of Linkedlist.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the findMergeNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def findMergeNode(head1, head2):
+    ptr1 = head1
+    ptr2 = head2
+    count = 0
+    while True:
+        count+=1 
+        if(ptr1==ptr2):
+            break
+        ptr1 = ptr1.next
+        ptr2 = ptr2.next
+        
+        if(ptr1==None):
+            ptr1 = head1
+        if(ptr2 == None):
+            ptr2 = head2
+   
+    return ptr2.data
+    
+    
+    
+    
+
+if __name__ == '__main__':
