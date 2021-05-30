1b9467feb0c0a3f31573be841b5b66225b9f91da
Rajeev Bandi <62938640+RajB07@users.noreply.github.com>
2021-05-30 12:23:33 +0530

    Add files via upload

---
+++ Ransom note_approach2.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+#
+# Complete the 'checkMagazine' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY magazine
+#  2. STRING_ARRAY note
+#
+
+def checkMagazine(magazine, note):
+    # Write your code here
+    counter1 = Counter(magazine)
+    counter2 = Counter(note)    
+    
+    if(counter2-counter1 == {}):
+        print("Yes")
+    else:
+        print("No")      
+    return 1
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    m = int(first_multiple_input[0])
+
+    n = int(first_multiple_input[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
