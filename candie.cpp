c76828cb03dc50910f66af58eb87ed452f0a69e9
Kadir Diwan <59998475+diwan-kadir@users.noreply.github.com>
2021-05-30 13:21:26 +0530

    Update candie.cpp

--- candie.cpp
+++ candie.cpp
@@ -1,45 +1,20 @@
 // LINK
 // https://www.hackerrank.com/challenges/candies/
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
 long candies(int n, vector<int> arr)
 {
-    long numOfCandies[n]={0};
-    int curr;
-    numOfCandies[0]=1;
-    for(int i = 1 ; i < n ; i++)
-    {
-        if (arr[i]>arr[i-1])
-        {
-            numOfCandies[i]=numOfCandies[i-1]+1;
-        }
-        else if (arr[i]<arr[i-1])
-        {
-            numOfCandies[i]=1;
-            curr = i;
-            if (numOfCandies[curr] == numOfCandies[curr-1]){
-                while (arr[curr] < arr[curr-1] and numOfCandies[curr-1] == numOfCandies[curr]){            curr -=1;
-                    numOfCandies[curr]++;
-                    
-                }
-            }
-
-        }
-        else 
-        {
-            numOfCandies[i]=1;
-        }
+    long numOfCandies[n],sum=0;
+    fill_n(numOfCandies,n,1);
     
-    }
-    long sum=0;
-    for(int i=0 ; i <n ; i++)
-    {
-        // cout << numOfCandies[i] << ' ' ;
-        sum+=numOfCandies[i];
-    }
+    for(int i = 1 ; i < n ; i++) 
+        if (arr[i]>arr[i-1]) numOfCandies[i]=numOfCandies[i-1]+1;
+    for(int i=n-1;i>0;i--)
+        if (arr[i-1] > arr[i] && numOfCandies[i-1] <= numOfCandies[i])
+            numOfCandies[i-1]=numOfCandies[i]+1;
+    for(int i=0 ; i <n ; i++) sum+=numOfCandies[i];
+    
     return sum;
 };
 
