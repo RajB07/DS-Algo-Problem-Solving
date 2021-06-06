1369b5f59137673b366eb04aed7d02f817b0a75c
Kadir Diwan <59998475+diwan-kadir@users.noreply.github.com>
2021-06-06 21:27:01 +0530

    Update Dynamic_Array.cpp

--- Dynamic_Array.cpp
+++ Dynamic_Array.cpp
@@ -21,14 +21,11 @@ int main() {
     for (int i = 0; i < Q; i ++) {
         cin >> type >> x >> y;
         seq = ((x ^ lastans) % N);
-       // cout << seq << endl;
         if (type == 1) {
             a[seq].push_back(y);
-           // cout << (seq) << " <-- " << y << endl;
         }
         else if (type == 2) {
             pos = (y % a[seq].size());
-            //cout << "pos " << pos << endl;
             lastans = a[seq][pos];
             cout << lastans << endl;
         }
