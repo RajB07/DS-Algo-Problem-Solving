ea479f21a906335e3cb2bd7d8d11ed6308b34a45
Kadir Diwan <59998475+diwan-kadir@users.noreply.github.com>
2021-06-16 23:17:14 +0530

    Update Sparse_Array.cpp

--- Sparse_Array.cpp
+++ Sparse_Array.cpp
@@ -1,22 +1,11 @@
 // LINK = https://www.hackerrank.com/challenges/sparse-arrays/problem
 
-
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
 string ltrim(const string &);
 string rtrim(const string &);
-
-/*
- * Complete the 'matchingStrings' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. STRING_ARRAY strings
- *  2. STRING_ARRAY queries
- */
 
 vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
     vector<int> a;
