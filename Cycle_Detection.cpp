82ada199ee8945d74fa932280c6404687490eec2
Kadir Diwan <59998475+diwan-kadir@users.noreply.github.com>
2021-06-16 22:05:53 +0530

    Create Cycle_Detection.cpp

---
+++ Cycle_Detection.cpp
@@ -0,0 +1,88 @@
+// LINK  https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the has_cycle function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool has_cycle(SinglyLinkedListNode* head) {
+    unordered_set <SinglyLinkedListNode*> address;
+    
+    while(head!=nullptr)
+    {
+        if(address.find(head) != address.end())
+            return 1;
+        address.insert(head);
+        head = head->next;
+        
+    }
+    return 0;
+
+}
+
+int main()
