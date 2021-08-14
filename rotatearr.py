f014ab714a5ffd91fcdb5c1e77b922493a8fde69
Rajeev Bandi <62938640+RajB07@users.noreply.github.com>
2021-08-14 22:54:23 +0530

    Add files via upload

---
+++ rotatearr.py
@@ -0,0 +1,20 @@
+
+nums = list(map(int,input().split()))
+low = nums[0]
+high = nums[-1]
+n = len(nums)
+k= int(input())
+def reverse(nums,low,high):
+    while(low<high):
+        nums[low],nums[high] = nums[high],nums[low] 
+        low+=1
+        high-=1
+
+def rotate(nums, k):
+    reverse(nums,0, n-k-1)
+    reverse(nums,n-k,n-1)
+    reverse(nums,0,n-1)
+
+rotate(nums,k)
+print(nums)
+
