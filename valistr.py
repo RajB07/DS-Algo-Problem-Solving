b7108a669641c96807593f6920b12c5fa8e04364
Rajeev Bandi <62938640+RajB07@users.noreply.github.com>
2021-08-15 23:57:21 +0530

    Add files via upload

---
+++ valistr.py
@@ -0,0 +1,27 @@
+#https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+
+from collections import Counter
+string=Counter(input())
+string=Counter(string.values())
+print(string)
+if len(string.keys())==1:
+    print("YES")
+
+elif len(string.keys())==2:
+    key1,key2=string.keys()
+    print(key1,key2)
+    if string[key1]==1 and (key1-1==key2 or key1-1==0):
+        print("YES")
+    elif string[key2]==1 and (key2-1==key1 or key2-1==0):
+        print("YES")
+    else:
+        print("NO")
+
+else:
+    print("NO")
+
+        
+
+
+
+
