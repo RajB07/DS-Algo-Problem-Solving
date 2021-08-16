cc4b4f216361a5ff6635466f00d69d643dfc8b8f
Rajeev Bandi <62938640+RajB07@users.noreply.github.com>
2021-08-16 15:52:57 +0530

    Update choclate feast.py

--- choclate feast.py
+++ choclate feast.py
@@ -1,1 +1,41 @@
 
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def chocolateFeast(n, c, m):
+    # Write your code here
+    chocapiece = n//c
+    wrapper = chocapiece
+    while wrapper>=m:
+        chocapiece+= wrapper//m
+        wrapper = wrapper//m + wrapper%m
+    
+    return chocapiece
+        
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        c = int(first_multiple_input[1])
+
+        m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
