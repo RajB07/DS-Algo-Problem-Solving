c4de01cb5db04a49a63cda6558157dfa8c8aca26
Rajeev Bandi <62938640+RajB07@users.noreply.github.com>
2021-08-18 23:45:47 +0530

    Add files via upload

---
+++ sumofdiageltsofmatrix.py
@@ -0,0 +1,17 @@
+rows = int(input("enter rows"))
+cols = int(input("enter cols"))
+mat = []
+for i in range(rows):
+    a = []
+    for j in range(cols): 
+        a.append(int(input()))
+    mat.append(a)
+
+print(mat)
+sum =0
+for i in range(rows):
+    for j in range(cols):
+        if(i==j or i+j ==rows-1):
+            sum+=mat[i][j]
+
+print(sum)
