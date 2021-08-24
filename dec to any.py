a015701007138379c76dedfe8bdced30a85ecb35
Rajeev Bandi <62938640+RajB07@users.noreply.github.com>
2021-08-24 23:07:44 +0530

    Add files via upload

---
+++ dec to any.py
@@ -0,0 +1,11 @@
+n = int(input())
+ans = 0
+powers = 1
+base= int(input())
+rem = 0
+while(n>0):
+    rem = int(n % base)
+    n = n/base
+    ans =  ans + rem*powers
+    powers = powers*10
+print(ans)
